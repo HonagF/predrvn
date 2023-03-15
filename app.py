6c83051552370b7cabd130fd471bd830610776ff
Nguyễn Minh Hoàng <69524738+HonagF@users.noreply.github.com>
2023-03-15 23:09:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ import pickle
 
 st.title('REVENUE PREDICTION')
 temp = st.number_input('Input Temperature')
+model = pickle.load(open('ice_scream_revenue', "rb"))
 if st.button('Predict'):
-  st.success('a')
+  st.success(model.predict(temp))
