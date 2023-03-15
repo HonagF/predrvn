2da31be44f49d840708570af9551491e0132b967
Nguyễn Minh Hoàng <69524738+HonagF@users.noreply.github.com>
2023-03-15 23:22:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import sklearn
 import pickle
 import numpy as np
 
-st.title('REVENUE PREDICTION')
-temp = st.number_input('Input Temperature')
+st.title('DU DOAN SO NAM DI TU CUA MAY')
+temp = st.number_input('NHAP SO DA MAY CO (G)')
 t = np.array(temp).reshape(-1,1)
 model = pickle.load(open('ice_scream_revenue', "rb"))
 rvn = model.predict(t)
 if st.button('Predict'):
-  st.write('Revenue Prediction')
-  st.success(float(rvn))
+  st.write('MAY BI BO TU')
+  st.success(float(rvn), ' NAM')
+  st.write('NGU NHU CON CHO')
